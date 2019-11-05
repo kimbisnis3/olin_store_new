f385d9524b6513cad2b2d7929147746219250b21
edo pratama <kimbisnis3@gmail.com>
2019-11-05 17:34:13 +0700

    update

--- application/helpers/ien_helper.php
+++ application/helpers/ien_helper.php
@@ -19,7 +19,7 @@ if (!function_exists('status')) {
 
     function lumise_url() {
         // return 'https://https://pabriktascustom.com/design/';
-        return 'http://localhost/lumise_new/';
+        return 'http://localhost/lumise_new_2/';
     }
 
     function ien($text)
