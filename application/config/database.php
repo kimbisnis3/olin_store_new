f4902862e73484912c5da5d55b304ab88ae2e315
edo pratama <kimbisnis3@gmail.com>
2019-11-07 13:57:01 +0700

    update

--- application/config/database.php
+++ application/config/database.php
@@ -70,27 +70,27 @@ $db['lumise'] = array(
 	'save_queries' => TRUE
 );
 
-$db['lumise_'] = array(
-	'dsn'	=> '',
-	'hostname' => 'cloud.gongsoft.com',
-	'username' => 'root',
-	'password' => 'gongsoft2016go',
-	'database' => 'pabriktascustomx',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
+// $db['lumise_'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'cloud.gongsoft.com',
+// 	'username' => 'root',
+// 	'password' => 'gongsoft2016go',
+// 	'database' => 'pabriktascustomx',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
 
 $db['default'] = array(
 	'dsn'	=> '',
